5766983091540e217ac94ce267768142aabc61c2
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-27 02:11:53 +0600

    idex.js

---
+++ index.js
@@ -0,0 +1,17 @@
+var number = [45, 68, 56, 87, 99, 12, 78];
+
+console.log(number);
+console.log(number.length);
+
+// get element value by index
+var element = number[2];
+console.log(element);
+
+// find the index of the element
+var positionIndex = number.indexOf(87);
+console.log(positionIndex);
+
+// set element value by index
+number[1] = 77;
+number[3] = 97;
+console.log(number);
