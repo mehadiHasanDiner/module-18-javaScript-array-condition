fac3dcea4c0ac6a2ecc6cd9fed17e70fbbc35f37
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-27 22:00:36 +0600

    nested-conditions.js

---
+++ nested-conditions.js
@@ -0,0 +1,19 @@
+var math = true;
+var geometry = false;
+var straightLine = true;
+
+if (math == true) {
+  if (geometry == true) {
+    if (straightLine == true) {
+      console.log("beshi beshi valo kaj korbi");
+    } else {
+      console.log("Baka pothe cholbi na");
+    }
+  } else {
+    console.log("Pithagoras hoyite parbi na");
+  }
+} else {
+  console.log("tui to kisui paros na");
+}
+
+console.log(6 >= 6);
