148ff6014d154ae1accbc485d8e974658eeca160
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-27 22:33:54 +0600

    Practice Problem 1 solved

---
+++ practice-problem1.js
@@ -0,0 +1,19 @@
+// practice problem 1
+var fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits);
+
+// indexof Banana
+var bananaIndex = fruits.indexOf("Banana");
+console.log(bananaIndex);
+
+// Adding Mango
+fruits[1] = "Mango";
+console.log(fruits);
+
+// Removing Orange
+fruits.pop("Orange");
+
+// Adding Watermelon
+fruits.push("Watermelon");
+
+console.log(fruits);
