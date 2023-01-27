3ecf6d387f21783f532149ac2bc8557c9749e737
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-27 23:18:19 +0600

    grade calculation

---
+++ practice-problem2.js
@@ -0,0 +1,22 @@
+// Grade calculation
+var examMarks = 90;
+// var Aliya = 95;
+// var Dhaliya = 66;
+// var Slaiya = 80;
+// var Maliya = 59;
+// var Liliya = 47;
+// var Jalaiya = 77;
+
+if (examMarks <= 49) {
+  console.log("Grade F");
+} else if (examMarks <= 59) {
+  console.log("Grade D");
+} else if (examMarks <= 69) {
+  console.log("Grade C");
+} else if (examMarks <= 79) {
+  console.log("Grade B");
+} else if (examMarks <= 89) {
+  console.log("Grade A");
+} else {
+  console.log("Grade A+");
+}
