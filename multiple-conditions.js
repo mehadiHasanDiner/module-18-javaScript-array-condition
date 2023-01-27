038efe77fc0d656fce80b817a006be3164824c86
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-27 20:27:12 +0600

    multiple-conditions.js git commit -m multiple-conditions.js git commit -m multiple-conditions.js

---
+++ multiple-conditions.js
@@ -0,0 +1,43 @@
+var isGraduated = false;
+var salary = 30000;
+cars = 0;
+
+// if (isGraduated == true) {
+//   console.log("eso diya kore feli");
+// }
+
+// if (isGraduated == true && salary >= 50000) {
+//   console.log("eso diya kore feli");
+// } else {
+//   console.log("tor kopale biya nai");
+// }
+
+// if (isGraduated == true && salary >= 50000 && cars >= 1) {
+//   console.log("eso diya kore feli");
+// } else {
+//   console.log("tor kopale biya nai");
+// }
+
+// if (isGraduated == true || salary >= 50000) {
+//   console.log("Eso prem kori");
+// } else {
+//   console.log("tomar friend er mobile number dao");
+// }
+
+// if (isGraduated == true || salary >= 50000 || cars >= 1) {
+//   console.log("Eso prem kori");
+// } else {
+//   console.log("tomar friend er mobile number dao");
+// }
+
+// if ((isGraduated == true && salary >= 50000) || cars >= 1) {
+//   console.log("Eso prem kori");
+// } else {
+//   console.log("tomar friend er mobile number dao");
+// }
+
+if (isGraduated == true && (salary >= 50000 || cars >= 1)) {
+  console.log("Eso prem kori");
+} else {
+  console.log("tomar friend er mobile number dao");
+}
