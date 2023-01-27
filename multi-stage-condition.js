6c2aca331387a8e7d5f6fd062951e473d2e303bf
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-27 20:56:58 +0600

    multi-stage-conditions file

---
+++ multi-stage-condition.js
@@ -0,0 +1,14 @@
+var money = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var tostBiscuit = 20;
+
+if (danishPrice < money) {
+  console.log("taratari den danish khamu");
+} else if (butterBread < money) {
+  console.log("butter bon khamu");
+} else if (tostBiscuit < money) {
+  console.log("chubai chubai toast biscuit khamu");
+} else {
+  console.log("Khali chai sather jonno valo");
+}
