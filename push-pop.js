762247a64f606e5d7f6895a135cd79370025f3eb
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-27 18:55:06 +0600

    push-pop.js

---
+++ push-pop.js
@@ -0,0 +1,15 @@
+// use push to get in an element in the last
+var numbers = [78, 45, 98, 45];
+numbers.push(63);
+// console.log(numbers);
+
+var friends = ["balam", "kalam", "salam"];
+friends.push("gelam");
+friends.push("pailam");
+console.log(friends);
+
+// use pop to get last element
+numbers.pop();
+console.log(numbers);
+var element = numbers.pop();
+console.log(element);
