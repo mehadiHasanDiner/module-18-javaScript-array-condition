d38b7ed6d1296d76be97c4a63cf70232630b3203
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-01-27 19:54:11 +0600

    conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,15 @@
+var iphonePrice = 90000;
+var myMoney = 98000;
+
+if (iphonePrice < myMoney) {
+  //   console.log("I will buy iphone");
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+
+if (chickenPrice < myMoney) {
+  console.log("chicken diya vat khabo");
+} else {
+  console.log("alu vorta diya vat khabo");
+}
