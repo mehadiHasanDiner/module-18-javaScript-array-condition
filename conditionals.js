14304fdab360d279bc7837d00403dcc905e4ed76
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-06-28 00:23:54 +0600

    update

--- conditionals.js
+++ conditionals.js
@@ -1,15 +1,41 @@
 var iphonePrice = 90000;
-var myMoney = 98000;
+var myMoney = 100000;
+
+// if iphone price is less than my budget. I will buy the iphone
+
+/**
+ * if iphone price is less than my budge
+ * i will buy the iphone
+ *
+ *
+ * if(condition){
+ *   //will execute if the condition is true
+ * }
+ *
+ * **/
 
 if (iphonePrice < myMoney) {
-  //   console.log("I will buy iphone");
+  console.log("iphone kine futani marbo");
 }
 
 var chickenPrice = 500;
 var myMoney = 50;
 
+// if (chickenPrice <= myMoney) {
+//   console.log("murgi diya vat khamu");
+// }
+// if (chickenPrice > myMoney) {
+//   console.log("Smashed potato and lentils soup");
+// }
+
+/**
+ * if chicken er price is less than my budge
+ * i will eat chicken
+ * na hoy: else
+ * */
+
 if (chickenPrice < myMoney) {
-  console.log("chicken diya vat khabo");
+  console.log("I will eat chicken");
 } else {
-  console.log("alu vorta diya vat khabo");
+  console.log("i will eat potato");
 }
