20bfd60b6b3a9d4aff862acbc2c7ea2852f07eec
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-06-30 00:15:28 +0600

    multi-stage-condition.js in js

--- multi-stage-condition.js
+++ multi-stage-condition.js
@@ -1,14 +1,14 @@
-var money = 10;
-var danishPrice = 45;
-var butterBread = 35;
-var tostBiscuit = 20;
+var money = 30;
+var danisPrice = 60;
+var butterbonPrice = 46;
+var tostBisucitePrice = 28;
 
-if (danishPrice < money) {
-  console.log("taratari den danish khamu");
-} else if (butterBread < money) {
-  console.log("butter bon khamu");
-} else if (tostBiscuit < money) {
-  console.log("chubai chubai toast biscuit khamu");
+if (danisPrice < money) {
+  console.log("dan dan danis khamu");
+} else if (butterbonPrice < money) {
+  console.log("dan butter bon Khamu");
+} else if (tostBisucitePrice < money) {
+  console.log("Chubai Chubai toast biscuite khamu");
 } else {
-  console.log("Khali chai sather jonno valo");
+  console.log("khali cha e sasther jonno valo");
 }
