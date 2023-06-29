38389618b62fdb2aab40f62eeef9649190d1f5c1
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-06-30 01:21:12 +0600

    practice problem 2

--- practice-problem2.js
+++ practice-problem2.js
@@ -1,22 +1,43 @@
-// Grade calculation
-var examMarks = 90;
-// var Aliya = 95;
-// var Dhaliya = 66;
-// var Slaiya = 80;
-// var Maliya = 59;
-// var Liliya = 47;
-// var Jalaiya = 77;
+// Grade Calculation
+var examMark = 80;
 
-if (examMarks <= 49) {
-  console.log("Grade F");
-} else if (examMarks <= 59) {
-  console.log("Grade D");
-} else if (examMarks <= 69) {
-  console.log("Grade C");
-} else if (examMarks <= 79) {
-  console.log("Grade B");
-} else if (examMarks <= 89) {
+// var myScore = 85;
+// var tom = 66;
+// var jane = 95;
+// var peter = 56;
+// var john = 40;
+
+if (examMark >= 80) {
   console.log("Grade A");
+} else if (examMark >= 60) {
+  console.log("Grade B");
+} else if (examMark >= 50) {
+  console.log("Grade C");
+} else if (examMark >= 40) {
+  console.log("Grade D");
 } else {
-  console.log("Grade A+");
+  console.log("Grade F");
 }
+
+// // Grade calculation
+// var examMarks = 90;
+// // var Aliya = 95;
+// // var Dhaliya = 66;
+// // var Slaiya = 80;
+// // var Maliya = 59;
+// // var Liliya = 47;
+// // var Jalaiya = 77;
+
+// if (examMarks <= 49) {
+//   console.log("Grade F");
+// } else if (examMarks <= 59) {
+//   console.log("Grade D");
+// } else if (examMarks <= 69) {
+//   console.log("Grade C");
+// } else if (examMarks <= 79) {
+//   console.log("Grade B");
+// } else if (examMarks <= 89) {
+//   console.log("Grade A");
+// } else {
+//   console.log("Grade A+");
+// }
